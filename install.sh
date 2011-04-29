133977709462fd9a646bfd12b13aeb39ed1f3433
ffeldhaus <florian.feldhaus@tu-dortmund.de>
2011-04-29 10:31:03 +0200

    * install script fixes

--- install.sh
+++ install.sh
@@ -18,48 +18,58 @@ else
     BIN_LOCATION=$ONE_LOCATION/bin
 fi
 
+echo -n "Stopping OCCI server"
+
 
 echo -n "Installing OCCI server."
+
+backup_information=""
 
 if [ -f $ONE_LOCATION/bin/occi-server ]; then
     mv $ONE_LOCATION/bin/occi-server $ONE_LOCATION/bin/occi-server.old
 fi
-
 cp bin/occi-server $ONE_LOCATION/bin
 
 echo -n "."
 
 if [ -f $ONE_LOCATION/etc/occi-server.conf ]; then
-  cp $ONE_LOCATION/etc/occi-server.conf $ONE_LOCATION/etc/occi-server.conf.bak
+  rm -f $ONE_LOCATION/etc/occi-server.conf.old
+  mv $ONE_LOCATION/etc/occi-server.conf $ONE_LOCATION/etc/occi-server.conf.old
+  backup_information=$backup_information"Your current occi-server.conf has been moved to occi-server.conf.old\n" 
 fi
-cp etc/occi-one.conf $ONE_LOCATION/etc
+cp etc/occi-server.conf $ONE_LOCATION/etc
 
 echo -n "."
 
 if [ -f $ONE_LOCATION/etc/occi-one.conf ]; then
-    cp $ONE_LOCATION/etc/occi-one.conf $ONE_LOCATION/etc/occi-one.conf.bak
+    rm -f $ONE_LOCATION/etc/occi-one.conf.old
+    mv $ONE_LOCATION/etc/occi-one.conf $ONE_LOCATION/etc/occi-one.conf.old
+    backup_information=$backup_information"Your current occi-one.conf has been moved to occi-one.conf.old\n"   
 fi
-cp etc/occi-server.conf $ONE_LOCATION/etc
+cp etc/occi-one.conf $ONE_LOCATION/etc
 
 if [ -d $ONE_LOCATION/etc/occi_one_templates ]; then
-    mv $ONE_LOCATION/etc/occi_one_templates $ONE_LOCATION/etc/occi_one_templates.bak
+    rm -rf $ONE_LOCATION/etc/occi_one_templates.old
+    mv -f $ONE_LOCATION/etc/occi_one_templates $ONE_LOCATION/etc/occi_one_templates.old
+    backup_information=$backup_information"Your current occi templates directory has been moved to occi_one_templates.old"
 fi
 cp -r etc/occi_one_templates $ONE_LOCATION/etc
 
 echo -n "."
 
-mv -f $ONE_LOCATION/lib/ruby/cloud/occi $ONE_LOCATION/lib/ruby/cloud/occi.old
+if [ -d $ONE_LOCATION/lib/ruby/cloud/occi ]; then
+  rm -rf $ONE_LOCATION/lib/ruby/cloud/occi.old
+  mv -f $ONE_LOCATION/lib/ruby/cloud/occi $ONE_LOCATION/lib/ruby/cloud/occi.old
+fi
+cp -r lib/ruby/cloud/occi/ $ONE_LOCATION/lib/ruby/cloud/occi
 
 echo -n "."
 
-cp -R lib/ruby/cloud/occi/* $ONE_LOCATION/lib/ruby/cloud/occi
-
+chmod +x $ONE_LOCATION/bin/occi-server
 chmod +x $ONE_LOCATION/lib/ruby/cloud/occi/occi-server.rb
 
-echo "."
+echo -n "."
 
 echo "done"
 
-echo "Your current occi-one.conf has been moved to $ONE_LOCATION/etc/occi-one.conf.bak"
-echo "Your current occi-server.conf has been moved to $ONE_LOCATION/etc/occi-server.conf.bak"
-echo "Your current occi templates have been moved to $ONE_LOCATION/etc/occi_one_templates.bak"
+echo -e $backup_information
