ef52eca05e633f11c0f23b7752b10b677a2dafdb
Florian Feldhaus <florian.feldhaus@tu-dortmund.de>
2011-10-27 15:29:20 +0200

    fix for OCCIRequest: only store kind, mixin, action if a category has been provided in request

--- lib/occi/rendering/http/OCCIRequest.rb
+++ lib/occi/rendering/http/OCCIRequest.rb
@@ -79,10 +79,10 @@ module OCCI
           $log.debug(@attributes)
 
           @categories = OCCI::CategoryRegistry.get_all(@parsed_categories).to_a
-          @kind = @categories.select {|category| category.kind_of?(OCCI::Core::Kind)}.last
-          @action_category = @categories.select {|category| category.kind_of?(OCCI::Core::Category)}.last
+          @kind = @categories.select {|category| category.kind_of?(OCCI::Core::Kind)}.last if @parsed_categories.length > 0
+          @action_category = @categories.select {|category| category.kind_of?(OCCI::Core::Category)}.last if @parsed_categories.length > 0
           @mixin = @parsed_categories.last
-          @mixins = @categories.select {|category| category.kind_of?(OCCI::Core::Mixin)}
+          @mixins = @categories.select {|category| category.kind_of?(OCCI::Core::Mixin)} if @parsed_categories.length > 0
         end
 
         def parse_json(body)
