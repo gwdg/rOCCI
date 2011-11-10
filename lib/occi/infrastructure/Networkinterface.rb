ab7884c355feaf292f43a9cdf544f08e2cab8819
Florian Feldhaus <florian.feldhaus@tu-dortmund.de>
2011-11-10 23:42:13 +0100

    fix to attributes for NetworkInterface

--- lib/occi/infrastructure/Networkinterface.rb
+++ lib/occi/infrastructure/Networkinterface.rb
@@ -44,9 +44,9 @@ module OCCI
         title   = "Networkinterface"
 
         attributes = OCCI::Core::Attributes.new()
-        attributes << OCCI::Core::Attribute.new(name = 'occi.networkinterface.interface', mutable = false,  mandatory = true, unique = true)
+        attributes << OCCI::Core::Attribute.new(name = 'occi.networkinterface.interface', mutable = false,  mandatory = false, unique = true)
         attributes << OCCI::Core::Attribute.new(name = 'occi.networkinterface.mac',       mutable = true,   mandatory = false, unique = true)
-        attributes << OCCI::Core::Attribute.new(name = 'occi.networkinterface.state',     mutable = false,  mandatory = true, unique = true)
+        attributes << OCCI::Core::Attribute.new(name = 'occi.networkinterface.state',     mutable = false,  mandatory = false, unique = true)
 
         KIND = OCCI::Core::Kind.new(actions, related, entity_type, entities, term, scheme, title, attributes)
         
