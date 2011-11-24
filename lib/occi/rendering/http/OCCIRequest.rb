31a2d87c8c5646f26efd2d525a2b52093473e30f
Florian Feldhaus <florian.feldhaus@tu-dortmund.de>
2011-11-24 15:27:56 +0100

    small fix to logging

--- lib/occi/rendering/http/OCCIRequest.rb
+++ lib/occi/rendering/http/OCCIRequest.rb
@@ -70,7 +70,7 @@ module OCCI
           
           $log.debug("HTTP Request Headers: ")
           request.env.each do |k,v|
-            $log.debug(k + ":" + v) if k.contains('HTTP')
+            $log.debug(k + ":" + v) if k.include?('HTTP')
           end
           $log.debug(request.POST)
           request.POST.values.each do |body|
