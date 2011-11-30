d6be25ba25ae8b7d4c0c8dc93ebe762a154d4671
Florian Feldhaus <florian.feldhaus@tu-dortmund.de>
2011-11-30 14:50:17 +0100

    fix to ipnetworking attributes

--- lib/occi/infrastructure/Ipnetworking.rb
+++ lib/occi/infrastructure/Ipnetworking.rb
@@ -39,9 +39,9 @@ module OCCI
         title   = "IP Network Mixin"
 
         attributes = OCCI::Core::Attributes.new()
-        attributes << OCCI::Core::Attribute.new(name = 'occi.network.address',    mutable = true, mandatory = false,   unique = true)
-        attributes << OCCI::Core::Attribute.new(name = 'occi.network.gateway',    mutable = true, mandatory = false,  unique = true)
-        attributes << OCCI::Core::Attribute.new(name = 'occi.network.allocation', mutable = true, mandatory = false,   unique = true)
+        attributes << OCCI::Core::Attribute.new(name = 'occi.networkinterface.address',    mutable = true, mandatory = false,   unique = true)
+        attributes << OCCI::Core::Attribute.new(name = 'occi.networkinterface.gateway',    mutable = true, mandatory = false,  unique = true)
+        attributes << OCCI::Core::Attribute.new(name = 'occi.networkinterface.allocation', mutable = true, mandatory = false,   unique = true)
           
         MIXIN = OCCI::Core::Mixin.new(term, scheme, title, attributes, actions, related, entities)
         
