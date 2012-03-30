b74cecd4c6e2df526ccad131528657edaedc222b
Florian Feldhaus <florian.feldhaus@udo.edu>
2012-03-30 08:36:02 +0200

    added spec for creating a compute resource

--- spec/occi/occi_server_spec.rb
+++ spec/occi/occi_server_spec.rb
@@ -1,20 +1,31 @@
 require 'occi/occi-server'
 require 'rspec'
 require 'rack/test'
+require 'logger'
 
 VERSION_NUMBER=0.5
 
-module OCCI
-  describe OCCIServer do
-    include Rack::Test::Methods
-    def app
-      OCCIServer
-    end
+describe OCCIServer do
+  include Rack::Test::Methods
 
-    it "serves registered categories through discovery interface" do
-      get '/-/'
-      last_response.should be_ok
-      last_response.body.should include('Category')
-    end
+  def app
+    OCCIServer.new({:log_level => Logger::UNKNOWN})
   end
+
+  it "serves registered categories through discovery interface" do
+    get '/-/'
+    last_response.should be_ok
+    last_response.body.should include('Category')
+  end
+
+  it "creates new compute resource" do
+    headers = {:accept => "text/uri-list"}
+    body = %Q{Category: compute; scheme="http://schemas.ogf.org/occi/infrastructure#"; class="kind";}
+    body += %Q{\nX-OCCI-Attribute: occi.compute.cores=2}
+    body += %Q{\nX-OCCI-Attribute: occi.compute.hostname="mycompute.example.com"}
+    post '/compute/', body, headers
+    last_response.should be_ok
+    last_response.body.should include('Location')
+  end
+
 end
