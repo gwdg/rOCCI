d527a11bd8b375edbafe74bf8766bf3a0b5f6cdd
Florian Feldhaus <florian.feldhaus@gwdg.de>
2012-06-08 15:50:38 +0200

    moved collection from OCCI::Core to OCCI and improved documentation and handling of collections

---
+++ lib/occi/collection.rb
@@ -0,0 +1,32 @@
+require 'hashie/mash'
+
+module OCCI
+  class Collection
+    attr_accessor :kinds
+    attr_accessor :mixins
+    attr_accessor :actions
+    attr_accessor :resources
+    attr_accessor :links
+
+    # Initialize a new OCCI Collection by initializing all supplied OCCI objects
+    #
+    # @param [Hash] collection including one or more of the keys kinds, mixins, actions, resources, links
+    def initialize(collection={})
+      @kinds = collection.kinds.collect { |kind| OCCI::Core::Kind.new(kind) } if collection.kinds.instance_of? Array
+      @mixins = collection.mixins.collect { |mixin| OCCI::Core::Mixin.new(mixin) } if collection.mixins.instance_of? Array
+      @actions = collection.actions.collect { |action| OCCI::Core::Action.new(action) } if collection.actions.instance_of? Array
+      @resources = collection.resources.collect { |resource| OCCI::Core::Resource.new(resource) } if collection.resources.instance_of? Array
+      @links = collection.links { |link| OCCI::Core::Link.new(link) } if collection.links.instance_of? Array
+    end
+
+    # @return [Array] categories combined list of all kinds, mixins and actions
+    def categories
+      @kinds + @mixins + @actions
+    end
+
+    # @return [Array] entities combined list of all resources and links
+    def entities
+      @resources + @links
+    end
+  end
+end
---
+++ spec/occi/collection_spec.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+require 'occi'
+
+module OCCI
+  describe Collection do
+    describe "initialize" do
+
+      it "should create a new OCCI collection including the OCCI base objects" do
+        collection = Hashie::Mash.new(:kinds => [], :mixins => [], :actions => [], :resources => [], :links => [])
+        collection.kinds << { :term => "compute", :scheme => "http://schemas.ogf.org/occi/infrastructure#" }
+        collection.mixins << { :term => "my_mixin", :scheme => "http://example.com/occi/tags#" }
+        collection.actions << { :term => "start", :scheme => "http://schemas.ogf.org/occi/infrastructure/compute/action#" }
+        collection.resources << { :kind => "http://schemas.ogf.org/occi/infrastructure#compute" }
+        collection.links << { :kind => "http://schemas.ogf.org/occi/infrastructure#storagelink" }
+      end
+
+    end
+  end
+end
