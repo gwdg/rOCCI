044168929208821b4fb228e8aefb0ee5b4a8c3b6
Florian Feldhaus <florian.feldhaus@gwdg.de>
2012-06-08 19:21:17 +0200

    updated OCCI Log scenario

--- spec/occi/log_spec.rb
+++ spec/occi/log_spec.rb
@@ -3,18 +3,14 @@ require 'occi/log'
 
 module OCCI
   describe Log do
-    describe "initialize" do
-      it "should initialize a new logger" do
-        OCCI::Log.new(STDOUT, OCCI::Log::INFO)
-      end
-    end
-  end
 
-  describe "log messages" do
-    it "should log into an IO object" do
-      r, w = IO.pipe
-      OCCI::Log.new(w, OCCI::Log::INFO)
-      OCCI::Log.info("Test")
+    describe "log messages" do
+      it "should log a message to a pipe" do
+        r, w = IO.pipe
+        OCCI::Log.new(w, OCCI::Log::INFO)
+        OCCI::Log.info("Test")
+        r.readline.include?("Test")
+      end
     end
   end
 end
