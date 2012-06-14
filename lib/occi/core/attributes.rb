721d942b1bfeec2c7d43116e3a428bee0b59864d
Florian Feldhaus <florian.feldhaus@gwdg.de>
2012-06-14 12:29:08 +0200

    added split method to OCCI::Attributes to create an OCCI::Attribues object out of a attribute in key value format

--- lib/occi/core/attributes.rb
+++ lib/occi/core/attributes.rb
@@ -5,7 +5,7 @@ module OCCI
     class Attributes < Hashie::Mash
 
       def combine
-        hash = {}
+        hash = { }
         self.each_key do |key|
           if self[key].kind_of? Hashie::Mash
             self[key].combine.each_pair { |k, v| hash[key + '.' + k] = v }
@@ -14,6 +14,20 @@ module OCCI
           end
         end
         return hash
+      end
+
+      def self.split(attributes)
+        attribute = Attributes.new
+        attributes.each do |name,value|
+          puts name
+          key, dot, rest = name.partition('.')
+          if rest.empty?
+            attribute[key] = value
+          else
+            attribute.merge! Attributes.new(key => self.split(rest => value))
+          end
+        end
+        return attribute
       end
 
     end
