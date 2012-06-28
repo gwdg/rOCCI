8fdfceabd0d3419e9b9e5d5360816095237474b9
Florian Feldhaus <florian.feldhaus@gwdg.de>
2012-06-28 11:03:59 +0200

    adapted link to changes in entity and removed unnecessary code

--- lib/occi/core/link.rb
+++ lib/occi/core/link.rb
@@ -1,5 +1,4 @@
 require 'hashie/mash'
-require 'occi/model'
 require 'occi/core/entity'
 require 'occi/core/kind'
 
@@ -7,60 +6,24 @@ module OCCI
   module Core
     class Link < Entity
 
-      # Define appropriate kind
-      def self.register
-        data = Hashie::Mash.new
-        data[:actions] = []
-        data[:related] = %w{http://schemas.ogf.org/occi/core#entity}
-        data[:term] = "link"
-        data[:scheme] = "http://schemas.ogf.org/occi/core#"
-        data[:title] = "Link"
+      # @return [OCCI::Core::Kind] kind definition of Link type
+      def self.kind_definition
+        kind = OCCI::Core::Kind.new('http://schemas.ogf.org/occi/core#','link')
 
-        data.attributes!.occi!.core!.target!.type = "string"
-        data.attributes!.occi!.core!.target!.pattern = ".*"
-        data.attributes!.occi!.core!.target!.required = false
-        data.attributes!.occi!.core!.target!.mutable = true
+        kind.related = %w{http://schemas.ogf.org/occi/core#entity}
+        kind.title = "Link"
 
-        data.attributes!.occi!.core!.source!.type = "string"
-        data.attributes!.occi!.core!.source!.pattern = ".*"
-        data.attributes!.occi!.core!.source!.required = false
-        data.attributes!.occi!.core!.source!.mutable = true
+        kind.attributes.occi!.core!.target!.type = "string"
+        kind.attributes.occi!.core!.target!.pattern = ".*"
+        kind.attributes.occi!.core!.target!.required = false
+        kind.attributes.occi!.core!.target!.mutable = true
 
-        kind = OCCI::Core::Kind.new(data)
-        OCCI::Model.register(kind)
-      end
+        kind.attributes.occi!.core!.source!.type = "string"
+        kind.attributes.occi!.core!.source!.pattern = ".*"
+        kind.attributes.occi!.core!.source!.required = false
+        kind.attributes.occi!.core!.source!.mutable = true
 
-      def target
-        return self[:target]
-      end
-
-      def target=(target)
-        self[:target] = target
-        self.attributes!.occi!.core!.target = target
-      end
-
-      def source
-        return self[:source]
-      end
-
-      def source=(source)
-        self[:source] = source
-        self.attributes!.occi!.core!.source = source
-      end
-
-      def convert_value(val, duping=false) #:nodoc:
-        case val
-          when self.class
-            val.dup
-          when ::Hash
-            val = val.dup if duping
-            self.class.subkey_class.new.merge(val) unless val.kind_of?(Hashie::Mash)
-            val
-          when Array
-            val.collect { |e| convert_value(e) }
-          else
-            val
-        end
+        kind
       end
 
     end
