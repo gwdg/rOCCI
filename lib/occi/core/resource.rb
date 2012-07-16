633d617f0a2be4983ca2ea1acc34e9ec3115bee1
Florian Feldhaus <florian.feldhaus@gwdg.de>
2012-07-16 21:34:10 +0200

    added method for getting and setting id and added a variable href to associate data (e.g. file content for storage) to the resource

--- lib/occi/core/resource.rb
+++ lib/occi/core/resource.rb
@@ -8,24 +8,40 @@ module OCCI
     class Resource < Entity
 
       attr_accessor :links
+      attr_accessor :href
 
       def initialize(kind, mixins=nil, attributes=nil, links=nil)
+        @href = nil
         @links = links.to_a
-        super(kind,mixins,attributes)
+        super(kind, mixins, attributes)
       end
 
       def self.kind_definition
-        kind = OCCI::Core::Kind.new('http://schemas.ogf.org/occi/core#','resource')
+        kind = OCCI::Core::Kind.new('http://schemas.ogf.org/occi/core#', 'resource')
 
         kind.related = %w{http://schemas.ogf.org/occi/core#entity}
-        kind.title = 'Resource'
+        kind.title   = 'Resource'
 
-        kind.attributes.occi!.core!.summary!.type = 'string'
-        kind.attributes.occi!.core!.summary!.pattern = '.*'
+        kind.attributes.occi!.core!.summary!.type     = 'string'
+        kind.attributes.occi!.core!.summary!.pattern  = '.*'
         kind.attributes.occi!.core!.summary!.required = false
-        kind.attributes.occi!.core!.summary!.mutable = true
+        kind.attributes.occi!.core!.summary!.mutable  = true
 
         kind
+      end
+
+      # set id for resource and update the the source of all links
+      # @param [UUIDTools::UUID] id
+      def id=(id)
+        super(id)
+        @links.each { |link| link.attributes.occi!.core!.source = self.location }
+      end
+
+      # update the source of all links before returning them
+      # @return [Array] links of resource
+      def links
+        @links.each { |link| link.attributes.occi!.core!.source = self.location }
+        @links
       end
 
       def as_json(options={ })
