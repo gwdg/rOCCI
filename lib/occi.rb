47d80af19aa7343899054c5094b748bf7c6272e0
Florian Feldhaus <florian.feldhaus@gwdg.de>
2012-09-03 22:42:41 +0200

    restructured occi requires

--- lib/occi.rb
+++ lib/occi.rb
@@ -1,18 +1,31 @@
+require 'hashie/mash'
+
+require 'active_support/json'
+require 'active_support/inflector'
+require 'active_support/notifications'
+
+require 'logger'
+
+require 'uuidtools'
+
+require 'httparty'
+
+require 'nokogiri'
+
+require 'rubygems/package'
+
+require 'zlib'
+
+require 'tempfile'
+
 require 'occi/version'
 require 'occi/parser'
 require 'occi/model'
 require 'occi/log'
 require 'occi/client'
 require 'occi/collection'
-require 'occi/core/action'
-require 'occi/core/attribute_properties'
-require 'occi/core/attributes'
-require 'occi/core/category'
-require 'occi/core/entity'
-require 'occi/core/kind'
-require 'occi/core/link'
-require 'occi/core/mixin'
-require 'occi/core/resource'
+require 'occi/core'
+require 'occi/infrastructure'
 
 require 'occiantlr/OCCIANTLRLexer'
 require 'occiantlr/OCCIANTLRParser'
