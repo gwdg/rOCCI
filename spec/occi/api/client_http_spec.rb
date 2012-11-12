6e3da1cc3b96da8105a83db3b5902972941614f8
Boris Parak <araxxxx@gmail.com>
2012-11-12 15:33:42 +0100

    RSpec refactoring

--- spec/occi/api/client_http_spec.rb
+++ spec/occi/api/client_http_spec.rb
@@ -10,79 +10,162 @@ module Occi
         use_vcr_cassette "client_http_text_plain"
 
         it "should establish connection" do
-          # TODO
+          expect{Occi::API::conn_helper}.to_not raise_error
         end
 
-        it "should list all available resources" do
+        it "should create a compute resource" do
+          client = Occi::API::conn_helper
+
+          client.get_resource "compute"
+          client.get_resource "http://schemas.ogf.org/occi/infrastructure#compute"
+        end
+
+        it "should create a network resource" do
+          client = Occi::API::conn_helper
+
+          client.get_resource "network"
+          client.get_resource "http://schemas.ogf.org/occi/infrastructure#network"
+        end
+
+        it "should create a storage resource" do
+          client = Occi::API::conn_helper
+
+          client.get_resource "storage"
+          client.get_resource "http://schemas.ogf.org/occi/infrastructure#storage"
+        end
+
+        it "should list all available resource types" do
+          client = Occi::API::conn_helper
+
+          client.get_resource_types
+        end
+
+        it "should list all available resource type identifiers" do
+          client = Occi::API::conn_helper
+
+          client.get_resource_type_identifiers
+        end
+
+        it "should list all available mixin types" do
+          client = Occi::API::conn_helper
+
+          client.get_mixin_types
+        end
+
+        it "should list all available mixin type identifiers" do
+          client = Occi::API::conn_helper
+
+          client.get_mixin_type_identifiers
         end
 
         it "should list compute resources" do
+          client = Occi::API::conn_helper
+
+          expect{client.list "compute"}.to_not raise_error
         end
 
         it "should list network resources" do
+          client = Occi::API::conn_helper
+
+          expect{client.list "network"}.to_not raise_error
         end
 
         it "should list storage resources" do
+          client = Occi::API::conn_helper
+
+          expect{client.list "storage"}.to_not raise_error
         end
 
         it "should list all available mixins" do
+          client = Occi::API::conn_helper
+
+          expect{client.get_mixins}.to_not raise_error
         end
 
         it "should list os_tpl mixins" do
+          client = Occi::API::conn_helper
+
+          expect{client.get_mixins "os_tpl"}.to_not raise_error
         end
 
         it "should list resource_tpl mixins" do
-        end
+          client = Occi::API::conn_helper
 
-        it "should describe all available resources" do
+          expect{client.get_mixins "resource_tpl"}.to_not raise_error
         end
 
         it "should describe compute resources" do
+          client = Occi::API::conn_helper
+
+          expect{client.describe "compute"}.to_not raise_error
         end
 
         it "should describe network resources" do
+          client = Occi::API::conn_helper
+
+          expect{client.describe "network"}.to_not raise_error
         end
 
         it "should describe storage resources" do
+          client = Occi::API::conn_helper
+
+          expect{client.describe "storage"}.to_not raise_error
         end
 
         it "should describe all available mixins" do
+          client = Occi::API::conn_helper
         end
 
         it "should describe os_tpl mixins" do
+          client = Occi::API::conn_helper
         end
 
         it "should describe resource_tpl mixins" do
+          client = Occi::API::conn_helper
         end
 
         it "should create a new compute resource" do
+          client = Occi::API::conn_helper
         end
 
         it "should create a new storage resource" do
+          # TODO
         end
 
         it "should create a new network resource" do
+          # TODO
+        end
+
+        it "should deploy an instance based on OVF/OVA file" do
+          # TODO
         end
 
         it "should delete a compute resource" do
         end
 
         it "should delete a network resource" do
+          # TODO
         end
 
         it "should delete a storage resource" do
+          # TODO
         end
 
         it "should trigger an action on a compute resource" do
+          # TODO
         end
 
         it "should trigger an action on a storage resource" do
+          # TODO
         end
 
         it "should trigger an action on a network resource" do
+          # TODO
         end
 
         it "should refresh its model" do
+          client = Occi::API::conn_helper
+          expect{client.refresh}.to_not raise_error
         end
       end
 
@@ -93,9 +176,6 @@ module Occi
           # TODO
         end
 
-        it "should list all available resources" do
-        end
-
         it "should list compute resources" do
         end
 
@@ -112,9 +192,6 @@ module Occi
         end
 
         it "should list resource_tpl mixins" do
-        end
-
-        it "should describe all available resources" do
         end
 
         it "should describe compute resources" do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,3 +13,24 @@ end
 RSpec.configure do |c|
     c.extend VCR::RSpec::Macros
 end
+
+module Occi
+  module API
+
+    def self.conn_helper
+      #Occi::API::ClientHttp.new('https://localhost:3300',
+      #                          { :type  => "none" },
+      #                          { :out   => "/dev/null",
+      #                            :level => Occi::Log::DEBUG })
+
+      Occi::API::ClientHttp.new("https://carach5.ics.muni.cz:11443",
+        { :type               => "x509",
+          :user_cert          => ENV['HOME'] + '/.globus/usercred.pem',
+          :user_cert_password => "",
+          :ca_path            => '/etc/grid-security/certificates' },
+        { :out   => STDERR,
+          :level => Occi::Log::DEBUG })
+    end
+
+  end
+end
