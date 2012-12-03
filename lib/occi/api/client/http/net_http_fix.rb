8136f15a09e09aaf824df933f1c0fdaab214115c
Boris Parak <araxxxx@gmail.com>
2012-12-03 13:00:15 +0100

    Fixed Net::HTTP hack compatibility
    * Compatible with 1.8.7, 1.9.2 and 1.9.3
    * Not compatible with jRuby, WIP

--- lib/occi/api/client/http/net_http_fix.rb
+++ lib/occi/api/client/http/net_http_fix.rb
@@ -1,10 +1,43 @@
-module Net
-  class HTTP
+##############################################################################
+## Net::HTTP hack allowing the use of X.509 proxy certificates.
+##############################################################################
 
-    SSL_ATTRIBUTES = SSL_ATTRIBUTES.concat %w(extra_chain_cert)
+# detect jRuby
+if RUBY_ENGINE == 'jruby'
+  #TODO: add jRuby support, this doesn't work
+  module Net
+    class HTTP
 
-    # An Array of certificates that will be sent to the client.
-    attr_accessor :extra_chain_cert
+      SSL_ATTRIBUTES = SSL_ATTRIBUTES.concat %w(extra_chain_cert)
 
+      # An Array of certificates that will be sent to the client.
+      attr_accessor :extra_chain_cert
+
+    end
+  end
+else
+  net_http_fix_rby_ver = RUBY_VERSION.split('.')
+
+  # detect old Rubies, tested with 1.8.7-p371
+  if net_http_fix_rby_ver[1] == "8"
+    module Net
+      class HTTP
+
+        # An Array of certificates that will be sent to the client.
+        ssl_context_accessor :extra_chain_cert
+
+      end
+    end
+  else
+    module Net
+      class HTTP
+
+        SSL_ATTRIBUTES = SSL_ATTRIBUTES.concat %w(extra_chain_cert)
+
+        # An Array of certificates that will be sent to the client.
+        attr_accessor :extra_chain_cert
+
+      end
+    end
   end
 end
