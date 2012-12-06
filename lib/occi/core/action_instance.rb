14286ec6fe3c3b58b29469386b89652a0ba5ebea
Florian Feldhaus <florian.feldhaus@gwdg.de>
2012-12-06 23:12:31 +0100

    Action instance
    - required Action_instance in core and changed order of requirements
    - updated Action_instance with reference to the Action describing it
    - added very basic test to collection spec
    
    #22

--- lib/occi/core/action_instance.rb
+++ lib/occi/core/action_instance.rb
@@ -2,9 +2,19 @@ module Occi
   module Core
     class Action_instance
 
+      class << self
+        attr_accessor :actions
+      end
+
       attr_accessor :action, :attributes, :model
 
-      def initialize(action, attributes={ })
+      @action = Occi::Core::Action.new('http://schemas.ogf.org/occi/core#', 'action_instance')
+
+      def initialize(action = self.action, attributes={ })
+        if action.kind_of? String
+          scheme, term = action.split '#'
+          action = Occi::Core::Action.new(scheme, term)
+        end
         @action     = action
         @attributes = attributes
       end
