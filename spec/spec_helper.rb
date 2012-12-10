8a506ed51298b378333ea53c004c659278e9982c
Maik Srba <msrba@gwdg.de>
2012-12-10 12:25:03 +0100

    commit rspecs for cloudplugfest

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,14 +3,14 @@ if ENV['COVERAGE']
   SimpleCov.start
 end
 
-require 'vcr'
-
-VCR.configure do |c|
-  c.hook_into :webmock
-  c.cassette_library_dir = 'spec/cassettes'
-  c.default_cassette_options = { :record => :new_episodes }
-end
-
-RSpec.configure do |c|
-    c.extend VCR::RSpec::Macros
-end
+#require 'vcr'
+#
+#VCR.configure do |c|
+#  c.hook_into :webmock
+#  c.cassette_library_dir = 'spec/cassettes'
+#  c.default_cassette_options = { :record => :new_episodes }
+#end
+#
+#RSpec.configure do |c|
+#    c.extend VCR::RSpec::Macros
+#end
