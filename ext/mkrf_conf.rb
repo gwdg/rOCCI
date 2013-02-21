0c036ddb1b2c02e33d2f6c7d70633574c33bacfe
Boris Parak <araxxxx@gmail.com>
2013-02-22 00:44:16 +0100

    Replaced release-related rake task
    Using rubygems-tasks instead of bundler

--- ext/mkrf_conf.rb
+++ ext/mkrf_conf.rb
@@ -5,7 +5,8 @@ require 'rubygems/dependency_installer.rb'
 begin
   Gem::Command.build_args = ARGV
 rescue NoMethodError
-  # do nothing
+  # do nothing but warn the user
+  warn "Gem::Command doesn't have a method named 'build_args'!"
 end 
 
 if defined? RUBY_PLATFORM && RUBY_PLATFORM == "java"
@@ -14,8 +15,8 @@ if defined? RUBY_PLATFORM && RUBY_PLATFORM == "java"
   begin
     inst.install "jruby-openssl" if ((defined? JRUBY_VERSION) && (JRUBY_VERSION.split('.')[1].to_i < 7))
   rescue
-    # installation failed
-    exit(1)
+    warn "Gem::DependencyInstaller failed to install 'jruby-openssl'!"
+    exit
   end
 end
 
